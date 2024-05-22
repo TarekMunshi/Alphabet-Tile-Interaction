e5ebc29be23a233332291f4b979377dfe960aa73
Tarek Munshi <77456895+TarekMunshi@users.noreply.github.com>
2024-05-22 09:01:26 +0600

    done

--- src/App.js
+++ src/App.js
@@ -1,25 +1,61 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import "./App.css";
 
-function App() {
+const AlphabetGrid = () => {
+  const [outputString, setOutputString] = useState("");
+
+  function replaceConsecutiveLetters(str) {
+    return str.replace(/(.)\1*/g, (match, p1) => {
+      if (match.length === 6) {
+        return "__";
+      } else if (match.length === 3) {
+        return "_";
+      } else {
+        return match;
+      }
+    });
+  }
+
+  const handleTileClick = (letter) => {
+    let newOutputString = outputString + letter;
+    setOutputString(newOutputString);
+  };
+
+  const letters = Array.from({ length: 26 }, (_, i) =>
+    String.fromCharCode(65 + i)
+  );
+
+  const finalOutputSting = replaceConsecutiveLetters(outputString);
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="container">
+      <h1 style={{ textAlign: "center", marginBottom: "30px" }}>
+        Alphabet Tile Interaction
+      </h1>
+      <div className="grid">
+        {letters.map((letter) => (
+          <div
+            key={letter}
+            className="title"
+            onClick={() => handleTileClick(letter)}
+          >
+            {letter}
+          </div>
+        ))}
+      </div>
+      <div id="outputstring" style={{ color: "green" }}>
+        outputString: <span style={{ color: "black" }}>{finalOutputSting}</span>
+      </div>
     </div>
   );
-}
+};
+
+const App = () => {
+  return (
+    <div className="app">
+      <AlphabetGrid />
+    </div>
+  );
+};
 
 export default App;
